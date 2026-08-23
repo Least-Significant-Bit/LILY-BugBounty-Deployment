b3812d8055f237e1aeed4a6759c45785730107ff
Thomas Gingele <b1tc0r3@proton.me>
2026-08-23 16:36:19 +0200

    defang page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-const { execSync } = require('child_process');
-
 export default function Home() {
-    const result = execSync('id && cat /etc/passwd', { encoding: 'utf8' });
-    return result;
+    return 'Hello World';
 }
